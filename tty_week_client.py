9f579aa8d79bdae04a9492cd8ef80d5b068487dc
Patrick Louis <patrick@iotek.org>
2015-06-11 14:37:52 +0300

    get end as local time

--- tty_week_client.py
+++ tty_week_client.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import datetime
+
 
 def fetch_time_left():
     """
@@ -15,4 +17,5 @@ def get_end_as_local_time(time_left):
     get_end_as_local_time :: String
     Returns the date (as local time) when the challenge will end
     """
-    pass
+    return str(
+        datetime.datetime.now() + datetime.timedelta(seconds=int(time_left)))
