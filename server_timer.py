a0ec6e528db4f07c138594a3ecfc0809e0725fd8
Patrick Louis <patrick@iotek.org>
2015-06-11 11:25:32 +0300

    api

--- server_timer.py
+++ server_timer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+import time
+
 LOCATION_OF_TIMER = "root/of/webserver/api"
 
 while 1:
     time_left = open(LOCATION_OF_TIMER).read()
     if (int(time_left) <= 0):
         break
-    open(LOCATION_OF_TIMER, 'w').write(int(time_left)-1)
+    open(LOCATION_OF_TIMER, 'w').write(str(int(time_left)-1))
     time.sleep(1)
 open(LOCATION_OF_TIMER, 'w').write("END!")
